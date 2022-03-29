9b06896932797be916fafa4b6c2c81269e18fcd3
om46608 <om46608@zut.edu.pl>
2022-03-30 01:55:59 +0200

    create login and register forms, improve header

--- src/app/login-register/register/register.component.ts
+++ src/app/login-register/register/register.component.ts
@@ -12,4 +12,11 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public submitRegister(name: string, username: string, email: string, password: string): void {
+    console.log('Name: ', name);
+    console.log('Username: ', username);
+    console.log('Email: ', email);
+    console.log('Password: ', password);
+  }
+
 }
