8bd0e79df58e9f753568459ecd986ce14352e991
om46608 <om46608@zut.edu.pl>
2022-03-29 21:57:06 +0200

    Add home module with routing and empty login/register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,19 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import {environment} from 'src/environments/environment.prod';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularFireDatabaseModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AngularFireDatabaseModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        NgbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
