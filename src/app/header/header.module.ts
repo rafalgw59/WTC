e63f8f30bd07cbd1cd97b8e6cdaad08fa8a47fcd
om46608 <om46608@zut.edu.pl>
2022-03-30 01:05:40 +0200

    add translation functionality

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import {RouterModule} from "@angular/router";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
     declarations: [
@@ -12,6 +13,7 @@ import {RouterModule} from "@angular/router";
     ],
     imports: [
       CommonModule,
+      TranslateModule.forChild(),
       RouterModule,
       RouterModule.forChild([
         {
